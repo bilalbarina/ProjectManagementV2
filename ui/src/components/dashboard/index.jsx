3ce52b25c74f5b57145f849ea5bd55546eaf408f
bilalbarina <dev.barina@gmail.com>
2022-12-16 09:13:56 +0100

    Updated dashboard components.

--- ui/src/components/dashboard/index.jsx
+++ ui/src/components/dashboard/index.jsx
@@ -4,9 +4,15 @@ import { Stats } from "./partials/Stats";
 export default function Dashboard() {
     return (
         <>
-            <h3 className="text-blue-700 font-semibold uppercase">
+        <div className="flex flex-row justify-between">
+        <h3 className="text-blue-700 font-semibold uppercase">
                 Tableau de borde d'état d'avancement
             </h3>
+            <div>
+                <input name="year_of_study" placeholder="Année"/>
+            </div>
+        </div>
+            
             <InfoCard />
             <Stats />
         </>
